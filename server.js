3246e7af36cc4fe5959f009a9b74ecfbcfd2fc22
emmaikpe <gowfinsoftware@gmail.com>
2025-05-21 08:38:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -108,7 +108,8 @@ if (!allowedTables.includes(type)) {
         res.json(result.recordset);
     } catch (err) {
         console.error(err);
-        res.status(500).send('Server Error');
+        res.status(500).send('err');
+        //res.status(500).send('Server Error');
     }
 });
 
