2e4dcf0a1f19e67b16e1c1195e921e28a69a737b
emmaikpe <gowfinsoftware@gmail.com>
2025-05-27 12:59:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { connectDB } = require('./db');
+const { connectDB,getPool, sql } = require('./db');
 const questionRoutes = require('./routes/questions');
+const scoreRoutes = require('./scores/import');
 
 const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://gowfin.github.io','https://cbt.ohafiamicrofinancebankplc.com']
+    origin: ['http://localhost:3000', 'https://gowfin.github.io','cbt.ohafiamicrofinancebankplc.com']
 }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api/questions', questionRoutes);
+app.use('/api/scores', scoreRoutes);
+ 
 const rateLimit = require('express-rate-limit');
 //security 
 const limiter = rateLimit({
@@ -23,11 +26,32 @@ const limiter = rateLimit({
 });
 
 app.use('/api/questions/import', limiter);
+app.use('/api/scores/import', limiter);
 // Health check
 app.get('/', (req, res) => {
   res.send('Quiz API Running');
 });
-
+app.post('/save',async(req,res)=>{
+  const {score,name,course}=req.body;
+  const semester = new Date().toISOString().slice(0, 10);
+  //  const semester='2025-05-27';
+ 
+    try {
+    const pool = await getPool();
+    await pool.request()
+            .input('studentId', sql.VarChar, name)
+            .input('courseCode', sql.VarChar, course)
+            .input('score', sql.Decimal(5, 2), score)
+            .input('semester', sql.VarChar, semester)
+            .query(`
+              INSERT INTO score (StudentId, CourseCode, Score, Semester)
+              VALUES (@studentId, @courseCode, @score, @semester)
+            `);
+            res.status(201).json({ success: true, message: 'Score added!' });
+  } catch (err) {
+     res.status(500).json({ success: false, error: err.message });
+  }          
+});
 // Start server
 const PORT = process.env.PORT || 5000;
 connectDB().then(() => {
