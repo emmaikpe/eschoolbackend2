99600aac718a57ad147a70954169bb4685bf6646
emmaikpe <gowfinsoftware@gmail.com>
2025-05-27 08:48:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const app = express();
 // Middleware
 
 app.use(cors({
-   origin: process.env.FRONTEND_URL || 'http://localhost:3000'
+   origin: process.env.FRONTEND_URL || 'http://localhost:3000' || https://gowfin.github.io/eschoolfrontend
+   || cbt.ohafiamicrofinancebankplc.com
 }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
